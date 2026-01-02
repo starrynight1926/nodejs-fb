72767437fd07c4536a47bab34db3b158f0b64527
starrynight <starrynight1926@gmail.com>
2026-01-02 14:19:32 +0700

    add server.js

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const { exec } = require('child_process');
-
 const app = express();
-app.use(express.json());
 
-app.post('/download', (req, res) => {
-  const url = req.body.url;
-  if (!url) return res.status(400).json({ error: 'Missing url' });
-
-  exec(`node fb_download.js "${url}"`, (err, stdout, stderr) => {
-    if (err) {
-      return res.status(500).json({ error: stderr });
-    }
-    res.json({ result: stdout });
-  });
+app.get('/', (req, res) => {
+  res.send('NodeJSFB API OK');
 });
 
-app.listen(201, () => {
-  console.log('API listening on port 201');
+app.listen(201, '127.0.0.1', () => {
+  console.log('Listening on 127.0.0.1:201');
 });
