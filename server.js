ada6b616d0017b61d3b137c00c1878591f1ead8a
starrynight <starrynight1926@gmail.com>
2026-01-02 14:30:22 +0700

    add server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
   res.send('NodeJSFB API OK');
 });
 
-app.listen(201, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3001');
+app.listen(3001, () => {
+  console.log('Listening on 0.0.0.0:3001');
 });
